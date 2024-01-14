32c6ffb59b394b6050d2d94791a40b1e458fb8db
MatyasPulchart <150050586+pulchart1@users.noreply.github.com>
2024-01-14 17:12:37 +0100

    Add files via upload

---
+++ ulohy/01/WA_11122023/WA_11122023/script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector("#menu ");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+})
